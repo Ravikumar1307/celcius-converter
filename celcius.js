32c67d34eca9fc6229be0ef4daaf508c3d0d0510
Ravikumar1307 <ravikumar13072003@gmail.com>
2024-07-17 17:52:15 +0530

    create celcius.html and celcius.js and style.css

---
+++ celcius.js
@@ -0,0 +1,6 @@
+function convert(){
+    var input=Number(document.getElementById("input").value)
+    var total=input*9/5+32
+    var result=document.getElementById("result")
+    result.innerHTML=total.toFixed(1)+ " fahrenheit"
+}
