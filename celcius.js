ad71850329cceb6540d7587b9b47fc4761564aeb
Ravikumar1307 <166548573+Ravikumar1307@users.noreply.github.com>
2024-07-17 19:56:36 +0530

    create celcius.js

--- celcius.js
+++ celcius.js
@@ -1,5 +1,5 @@
 function convert(){
-    var input=Number(document.getElementById("input").value)
+     var input=Number(document.getElementById("input").value)
     var total=input*9/5+32
     var result=document.getElementById("result")
     result.innerHTML=total.toFixed(1)+ " fahrenheit"
